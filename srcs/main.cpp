d490ae56cfe570c0bbdd6d939c7238f0fdd27b9a
kit234 <kitorjason@sina.com>
2022-10-06 08:12:04 +0800

    Update

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -7,23 +7,25 @@
 using namespace std;
 using namespace jsonparser;
 
-class Student :public JsonObject {
-public:
-	Student(const std::string& name,int age){
-		this->set("name",new JsonString(name));
-		this->set("age",new JsonNumber(age));
-	}
-};
-
 int main(){
 	JsonObject* school=new JsonObject; // 新建一个JsonObject对象
 	JsonArray* students=nullptr;
-	school->create_array("students")->to_array("students",&students) // 新建"students"的数组字段,并赋值给students变量
-		->set("name",new JsonString("University")); // 新建一个name字段
-	students->push_back(new Student("kit",18))      // 为students字段添加学生
-		->push_back(new Student("kitty",20))
-		->push_back(new Student("world",16));
-	std::cout<<school->to_json_string()<<std::endl;
+	school->create_array("students")->out_array("students",&students)->get_array("students") // 新建students字段数组并赋值给变量,然后进入字段
+			->create_back_object()->get_back_object()  // 在students中添加第一个学生信息,并进入字段
+				->set("name",new JsonString("kit"))
+				->set("age",new JsonNumber(18))
+				->set("sex",new JsonString("m"))
+			->move_out_array()  // 返回students字段
+			->create_back_object()->get_back_object()  // 在students字段中添加第二个学生,并进入字段
+				->set("name",new JsonString("kitty"))
+				->set("age",new JsonNumber(20))
+				->set("sex",new JsonString("m"))
+			->move_out_array()  // 返回students字段
+		->move_out_object() // 返回最外层的JsonObject对象
+		->set("year",new JsonString("2019-9-16"));  // 新建year字段
+
+	std::cout<<"Student Total: "<<students->size()<<std::endl;
+	std::cout<<school->to_json_string()<<std::endl; // Json对象转Json字符串
 	delete school;
 	return 0;
  }
