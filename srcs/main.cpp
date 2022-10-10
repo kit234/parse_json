d41a6cbc7efad81afff24915d26fd087225714e4
BakakaBa <36580946+kit234@users.noreply.github.com>
2022-10-10 23:01:58 +0800

    add literal and ignore blank (#5)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -12,17 +12,17 @@ int main(){
 	JsonArray* students=nullptr;
 	school->create_array("students")->out_array("students",&students)->get_array("students") // 新建students字段数组并赋值给变量,然后进入字段
 			->create_back_object()->get_back_object()  // 在students中添加第一个学生信息,并进入字段
-				->set("name",new JsonString("kit"))
-				->set("age",new JsonNumber(18))
-				->set("sex",new JsonString("m"))
+				->set("name",R"("kit")"_json_string)
+				->set("age",18_json_number)
+				->set("sex",R"("m")"_json_string)
 			->move_out_array()  // 返回students字段
 			->create_back_object()->get_back_object()  // 在students字段中添加第二个学生,并进入字段
-				->set("name",new JsonString("kitty"))
-				->set("age",new JsonNumber(20))
-				->set("sex",new JsonString("m"))
+				->set("name",R"("kitty")"_json_string)
+				->set("age",20_json_number)
+				->set("sex",R"("m")"_json_string)
 			->move_out_array()  // 返回students字段
 		->move_out_object() // 返回最外层的JsonObject对象
-		->set("year",new JsonString("2019-9-16"));  // 新建year字段
+		->set("year",R"("2019-9-16")"_json_string);  // 新建year字段
 
 	std::cout<<"Student Total: "<<students->size()<<std::endl;
 	std::cout<<school->to_json_string()<<std::endl; // Json对象转Json字符串
